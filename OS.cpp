225fbea018b8c8d466a1acb8eb9c9418a772a78d
ryo1120ryo <35241592+ryo1120ryo@users.noreply.github.com>
2019-12-20 23:37:23 +0900

    Add files via upload

--- OS.cpp
+++ OS.cpp
@@ -4,9 +4,13 @@
 
 int main() {
 	FILE* fp;
-	int j = 0;
+	
+	int i, j = 0, t = 0;
 	int select;
 	char output[28] = { 0 };
+
+	int count;
+	char Alphabet;
 
 	printf("SELECT  1 or 2\n"
 		"1...円周率\n"
@@ -17,16 +21,16 @@ int main() {
 		{
 			//pi.txt読み込み
 			fp = fopen("pi.txt", "r");
-		}
-		else
-		{
+		}else{
 			fp = fopen("route2.txt", "r");
 		}
+
 			if (fp == NULL)
 			{
 				printf("ファイルオープン失敗\n");
 				return -1;
 			}
+		
 			// 配列piで読み込み表示
 			int input[28] = { 0 };
 			while ((input[j] = fgetc(fp)) != EOF)
@@ -34,5 +38,19 @@ int main() {
 				input[j] = input[j] - 48;
 				printf("%d", input[j]);
 				j++;
+			}
+
+			while (t=28)
+			{
+				for (j = 0; j < 28; j++)
+				{
+					count = input[j];
+				}
+				for (Alphabet = 'A'; Alphabet <= 'W'; Alphabet++) {
+					for (i = 0; i <= count; i++) {
+						output[i] = Alphabet;
+					 }
+				}
+				t++;
 			}
 }
